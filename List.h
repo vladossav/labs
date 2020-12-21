b7c8950f044eebf25c8b5d168a6367396d0db6b8
Vladislav Savenkov <71334457+vladossav@users.noreply.github.com>
2020-12-21 21:05:12 +0700

    Add files via upload

--- List.h
+++ List.h
@@ -1,26 +1,215 @@
 #pragma once
+#include <iostream>
 #include "ArrayWork.h"
+using namespace std;
+
+template <typename T>
+class List;
+
+template <typename T>
 class Node {
-	friend class List;
+	friend class List<T>;
 private:
-	ArrayWork data;
-	Node * next, * prev;
+	T data;
+	Node<T>* next, * prev;
 };
 
+template <typename T>
 class List {
 private:
-	Node* head, * tail;
+	Node<T>* head, * tail;
 	int count;
 public:
-	List();
-	~List();
-	void add(ArrayWork data);
-	void insert(int pos, ArrayWork data);
-	void viewAll();
-	void view(int pos);
-	void delAll();
-	void del(int pos);
-	bool isEmpty();
-	int search(int num);
-	ArrayWork getData(int pos);
+	List() {
+		head = tail = NULL;
+		count = 0;
+	}
+	~List() {
+		delAll();
+	}
+
+	void add(T data) {
+		Node<T>* temp = new Node<T>;
+		if (head == NULL || tail == NULL) {
+			temp->next = temp;
+			temp->prev = temp;
+			temp->data = data;
+			head = tail = temp;
+			count++;
+			return;
+		}
+		temp->next = tail;
+		temp->data = data;
+		temp->prev = head;
+		tail->prev = temp;
+		head->next = temp;
+		tail = temp;
+		count++;
+		return;
+	}
+
+	void insert(int pos, T data) {
+		if (pos < 1 || pos > count + 1) {
+			throw runtime_error("Wrong position");
+		}
+
+		Node<T>* temp = new Node<T>;
+		if (head == NULL || tail == NULL) {
+			temp->next = temp;
+			temp->prev = temp;
+			temp->data = data;
+			head = tail = temp;
+			count++;
+			return;
+		}
+
+		if (pos == 1 && head != NULL && tail != NULL) {
+			temp->next = tail;
+			temp->data = data;
+			temp->prev = head;
+			head->next = temp;
+			tail->prev = temp;
+			head = temp;
+			count++;
+			return;
+		}
+
+		if (pos == count + 1) {
+			temp->next = tail;
+			temp->data = data;
+			temp->prev = head;
+			tail->prev = temp;
+			head->next = temp;
+			tail = temp;
+			count++;
+			return;
+		}
+
+		int i = 1;
+		Node<T>* Ins = head;
+		while (i < pos) {
+			Ins = Ins->prev;
+			i++;
+		}
+		Node<T>* NextIns = Ins->next;
+		NextIns->prev = temp;
+		temp->next = NextIns;
+		temp->prev = Ins;
+		Ins->next = temp;
+		temp->data = data;
+		count++;
+	}
+
+	void viewAll() {
+		if (count == 0) {
+			throw runtime_error("List is empty");
+		}
+		Node<T>* temp = head;
+		for (int i = 1; i < count; i++) {
+			cout << temp->data << endl;
+			temp = temp->prev;
+		}
+		cout << temp->data << endl;
+	}
+
+	void view(int pos) {
+		Node<T>* temp;
+		temp = head;
+		int i = 1;
+
+		while (i < pos) {
+			temp = temp->prev;
+			i++;
+		}
+		cout << temp->data << endl;
+	}
+
+	void delAll() {
+		while (count != 0)
+			del(1);
+	}
+
+	void del(int pos) {
+		if (pos < 1 || pos > count + 1) {
+			throw runtime_error("Wrong position");
+		}
+		if (count == 1) {
+			Node<T>* del = head;
+			delete del;
+			count--;
+			return;
+		}
+
+		if (pos == 1) {
+			Node<T>* del = head;
+			Node<T>* prevdel = del->prev;
+			Node<T>* nextdel = del->next;
+			prevdel->next = nextdel;
+			nextdel->prev = prevdel;
+			head = prevdel;
+			delete del;
+			count--;
+			return;
+		}
+
+		if (pos == count + 1) {
+			Node<T>* del = tail;
+			Node<T>* nextdel = del->next;
+			Node<T>* prevdel = del->prev;
+			prevdel->next = nextdel;
+			nextdel->prev = prevdel;
+			head = nextdel;
+			delete del;
+			count--;
+			return;
+		}
+
+		int i = 1;
+		Node<T>* del = head;
+		while (i < pos) {
+			del = del->prev;
+			i++;
+		}
+
+		Node<T>* prevdel = del->prev;
+		Node<T>* nextdel = del->next;
+		prevdel->next = nextdel;
+		nextdel->prev = prevdel;
+		delete del;
+		count--;
+	}
+
+	bool isEmpty() {
+		if (count == 0) {
+			return 0;
+		}
+		else {
+			return 1;
+		}
+	}
+	int search(int num) {
+		Node<T>* temp;
+		temp = head;
+		T buff;
+		for (int i = 1; i < count; i++) {
+			buff = temp->data;
+
+			if (buff.search(num) == -1)
+				temp = temp->prev;
+			else
+				return i;
+		}
+		return -1;
+	}
+	T getData(int pos) {
+		Node<T>* temp;
+		temp = head;
+		int i = 1;
+
+		while (i < pos) {
+			temp = temp->prev;
+			i++;
+		}
+		return temp->data;
+	}
 };
--- main.cpp
+++ main.cpp
@@ -2,70 +2,47 @@
 #include "ArrayWork.h"
 #include "StackArray.h"
 #include "QueueArray.h"
-#include <iostream>
-#include <ctime>
 using namespace std;
 
-bool testAdd() {	
-	List list;
-	ArrayWork obj1;
-	list.add(obj1);
-	if (list.getData(1) == obj1) return 0;
-	else return 1;
-}
-
-bool testInsert() {
-	int A[] = { 8,-2,56,4 };
-	List list;
-	StackArray obj(4, A);
-	StackArray obj1;
-	QueueArray obj2;
-	ArrayWork obj3;
-	list.add(obj1);
-	list.add(obj2);
-	list.add(obj3);
-	list.insert(2, obj);
-	if (list.getData(2) == obj)
-		if (list.getData(3) == obj2)
-			return 0;		
-	return 1;
-}
-
-bool testEmpty() {
-	List list;
-		if (list.isEmpty() != 0) return 1;
-	ArrayWork obj;
-	list.add(obj);
-	if (list.isEmpty() != 1) return 1;
+bool testInt() {
+	List <int> list;
+	int A[] = { -10,15,0,3999,67 };
+	
+	for (int i = 0; i < 5; i++) {
+		list.add(A[i]);
+	}
+	for (int i = 0; i < 5; i++) {
+		if (!(list.getData(i + 1) == A[i])) return 1;
+	}
 	return 0;
 }
 
-bool testDelete() {
-	List list;
-	ArrayWork obj;
-	StackArray obj1;
-	QueueArray obj2;
+bool testDouble() {
+	List <double> list;
+	double A[] = { -10.54,15.32,0,3999.9223,67 };
+	
+	for (int i = 0; i < 5; i++) {
+		list.add(A[i]);
+	}
+	for (int i = 0; i < 5; i++) {
+		if (!(list.getData(i + 1) == A[i])) return 1;
+	}
+	return 0;
+}
+
+bool testArrayWork() {
+	List <ArrayWork> list;
+	int A[] = { -10,15,0,3999,67 };
+	ArrayWork obj1(5,A);
+	StackArray obj2;
+	QueueArray obj3;
+
 	list.add(obj1);
-	list.add(obj);
 	list.add(obj2);
-	list.del(2);
+	list.add(obj3);
 	if (!(list.getData(1) == obj1)) return 1;
 	if (!(list.getData(2) == obj2)) return 1;
-	return 0;
-}
-
-bool testSearch() {
-	List list;
-	int A[] = { 8,-2,77777,4 };
-	ArrayWork obj1;
-	StackArray obj2(4,A);
-	QueueArray obj3;
-	list.add(obj1);
-	list.add(obj2);
-	list.add(obj3);
-	
-	if (list.search(77777) != 2) return 1;
-	if (list.search(91919119) != -1) return 1;
+	if (!(list.getData(3) == obj3)) return 1;
 	return 0;
 }
 
@@ -73,11 +50,9 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 	
 	cout << "  0 Successfully || 1 Failed";
-	cout << "\ntest add: " << testAdd();
-	cout << "\ntest insert: " << testInsert();
-	cout<< "\ntest empty: " << testEmpty();
-	cout << "\ntest del: " << testDelete();
-	cout << "\ntest search: " << testSearch();
+	cout << "\ntest template <int>: "<<testInt();
+	cout << "\ntest template <double>: " << testDouble();
+	cout << "\ntest template <ArrayWork>: " << testArrayWork();
 	cout << endl;
 
 	return 0;
